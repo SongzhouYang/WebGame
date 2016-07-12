a54f0f81910ae3890a8027043284b0ecbd7b494f
Tauta <zhr14@mails.tsinghua.edu.cn>
2016-07-13 06:11:41 +0800

    change menu image

--- app/scripts/StartMenu.js
+++ app/scripts/StartMenu.js
@@ -11,7 +11,7 @@ WebGame.StartMenu.prototype = {
   create: function () {
     this.startbackground = this.add.image(this.world.centerX, this.world.centerY, 'menu');
     this.startbackground.anchor.setTo(0.5);
-    this.startButton = this.add.button(this.world.centerX, this.world.centerY + 300, 'StartButton', this.startGame, this);
+    this.startButton = this.add.button(this.world.centerX, this.world.centerY + 200, 'StartButton', this.startGame, this);
     this.startButton.anchor.setTo(0.5);
     this.camera.follow(this.startbackground);
     this.music = this.add.audio('menu', undefined, true);
