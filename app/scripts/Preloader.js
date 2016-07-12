4af442b1b7026f109d6ecda014f0bf35807e8803
Songzhou Yang <yangsongzhou@outlook.com>
2016-07-12 16:08:28 +0800

    add start button

--- app/scripts/Preloader.js
+++ app/scripts/Preloader.js
@@ -1,11 +1,11 @@
-WebGame.Preloader = function() {
+WebGame.Preloader = function () {
   this.preloaderBar = null;
   this.ready = false;
   this.levelone = null;
 };
 
 WebGame.Preloader.prototype = {
-  preload: function() {
+  preload: function () {
     this.preloaderBar = this.add.sprite(this.world.centerX, this.world.centerY, 'preloaderBar');
     this.preloaderBar.anchor.setTo(0.5);
     this.load.setPreloadSprite(this.preloaderBar);
@@ -13,18 +13,19 @@ WebGame.Preloader.prototype = {
     // load everything for the game here
     this.load.image('square', 'images/bluesquare.png');
     this.load.image('menu', 'images/menu.png');
+    this.load.image('StartButton', 'images/StartButton.png');
 
     this.load.tilemap('gamemap', 'images/gamemap.json', null, Phaser.Tilemap.TILED_JSON);
     this.load.image('tiles_spritesheet', 'images/tileset/tiles_spritesheet.png');
     this.load.audio('music', 'images/BaseAfterBase.mp3');
   },
 
-  create: function() {
+  create: function () {
     this.preloaderBar.cropEnabled = false;
   },
 
-  update: function() {
+  update: function () {
     this.ready = true;
     this.state.start('StartMenu');
-  },
+  }
 };
--- app/scripts/StartMenu.js
+++ app/scripts/StartMenu.js
@@ -1,20 +1,19 @@
-WebGame.StartMenu = function(game) {
+WebGame.StartMenu = function (game) {
   this.startbackground = null;
   this.startPrompt = null;
 };
 
 WebGame.StartMenu.prototype = {
-  create: function() {
+  create: function () {
     this.startbackground = this.add.image(this.world.centerX, this.world.centerY, 'menu');
     this.startbackground.anchor.setTo(0.5);
     this.startbackground.inputEnabled = true;
-    this.startbackground.events.onInputDown.addOnce(this.startGame, this);
-    
-    // this.startPrompt = this.add.bitmapText(this.world.centerX-155, this.world.centerY+180, 'Arial', 'Touch to Start', 20);
+    this.add.button(this.world.centerX - 50, this.world.centerY + 280, 'StartButton', this.startGame, this);
+
 
   },
 
-  startGame: function(pointer) {
+  startGame: function (pointer) {
     this.state.start('Game');
   }
 };
